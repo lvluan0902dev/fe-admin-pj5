3f4614315703b6bb83af32967f213be5e190ae1b
Le Van Luan <lvluan0902dev@gmail.com>
2022-10-18 23:28:28 +0700

    Structure init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('src/app/main/main.module').then((m) => m.MainModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    SharedModule
+  ]
+})
+export class MainModule { }
