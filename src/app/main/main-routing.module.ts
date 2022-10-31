e9adbac136571299ebb3f1e0363c68f79e1ce9e9
Le Van Luan <lvluan0902dev@gmail.com>
2022-10-31 21:40:23 +0700

    Fix error when refresh url

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -20,16 +20,25 @@ const routes: Routes = [
         component: DashboardComponent
       },
       {
-        path: 'slider/list',
-        component: SliderListComponent
-      },
-      {
-        path: 'slider/add',
-        component: SliderAddComponent
-      },
-      {
-        path: 'slider/edit',
-        component: SliderEditComponent
+        path: 'slider',
+        children: [
+          {
+            path: '',
+            component: SliderListComponent
+          },
+          {
+            path: 'list',
+            component: SliderListComponent
+          },
+          {
+            path: 'add',
+            component: SliderAddComponent
+          },
+          {
+            path: 'edit',
+            component: SliderEditComponent
+          }
+        ]
       }
     ]
   }
