2f508e5fa6271badeb61b73a083f9e13fd1f5a1e
Le Van Luan <lvluan0902dev@gmail.com>
2022-11-01 22:11:16 +0700

    Slider - Create Add And Back

--- src/app/main/components/slider/slider-add/slider-add.component.ts
+++ src/app/main/components/slider/slider-add/slider-add.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { FormBuilder, Validators } from '@angular/forms';
 import { SliderService } from 'src/app/main/services/slider/slider.service';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider-add',
@@ -22,7 +24,9 @@ export class SliderAddComponent implements OnInit {
   constructor(
     private title: Title,
     private fb: FormBuilder,
-    private sliderService: SliderService
+    private sliderService: SliderService,
+    private toastService: ToastService,
+    private router: Router
   ) {
     this.title.setTitle('Slider add');
   }
@@ -43,7 +47,38 @@ export class SliderAddComponent implements OnInit {
       }
 
       this.sliderService.add(formData).subscribe((response) => {
-        console.log(response);
+        if (response.success == 1) {
+          this.toastService.success('Added successfully', response.message);
+          this.submitted = false;
+          this.form.reset();
+        } else {
+          this.toastService.error('Have something error', response.message);
+        }
+      });
+    } else {
+      console.log(this.form);
+    }
+  }
+
+  public saveAndBack(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      this.sliderService.add(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Success', response.message);
+          this.router.navigateByUrl('/slider/list');
+        } else {
+          this.toastService.error('Error', response.message);
+        }
       });
     } else {
       console.log(this.form);
