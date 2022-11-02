d911595af6c8a080a8e3ec1e2aa2aa0ae9573db3
leluan <levanluan090201>
2022-11-02 16:48:40 +0700

    Core - Add method, improve code http service

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -7,30 +7,34 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class HttpService {
-  private host = environment.apiUrl
+  private host = environment.apiUrl;
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
   public get(api: string, options?: any): Observable<any> {
-    const url = `${this.host}/${api}`
+    const url = `${this.host}/${api}`;
     return this.httpClient.get(url, options);
   }
 
   public post(api: string, payload: any, options?: any): Observable<any> {
-    const url = `${this.host}/${api}`
-    return this.httpClient.post(url, payload, options)
+    const url = `${this.host}/${api}`;
+    return this.httpClient.post(url, payload, options);
   }
 
-  public put() {
-
+  public put(api: string, payload: any, options?: any): Observable<any> {
+    const url = `${this.host}/${api}`;
+    return this.httpClient.put(url, payload, options);
   }
 
-  public patch() {
-
+  public patch(api: string, payload: any, options?: any): Observable<any> {
+    const url = `${this.host}/${api}`;
+    return this.httpClient.patch(url, payload, options);
   }
 
-  public delete() {
-
+  public delete(api: string, options?: any): Observable<any> {
+    const url = `${this.host}/${api}`;
+    return this.httpClient.delete(url, options);
   }
 }
