da438504608171d9499cfdfc0949a0f9e8a63ca0
leluan <levanluan090201>
2022-11-03 09:16:56 +0700

    Testimonial - Add model

---
+++ src/app/main/models/testimonial/testimonial.model.ts
@@ -0,0 +1,10 @@
+export interface Testimonial {
+    id: number,
+    name: string,
+    content: string,
+    image_name: string,
+    image_path: string,
+    status: number,
+    created_at: string,
+    updated_at: string
+}
