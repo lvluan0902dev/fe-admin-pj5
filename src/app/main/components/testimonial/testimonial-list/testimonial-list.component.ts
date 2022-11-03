d1aaa5db527b0d2b9a30fe3eee38d2def26f58bf
leluan <levanluan090201>
2022-11-03 09:52:39 +0700

    Testimonial - Create list interface

--- src/app/main/components/testimonial/testimonial-list/testimonial-list.component.ts
+++ src/app/main/components/testimonial/testimonial-list/testimonial-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ConfirmationService, LazyLoadEvent } from 'primeng/api';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { Testimonial } from 'src/app/main/models/testimonial/testimonial.model';
+import { TestimonialService } from 'src/app/main/services/testimonial/testimonial.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-testimonial-list',
@@ -6,10 +12,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonial-list.component.css']
 })
 export class TestimonialListComponent implements OnInit {
+  public url = environment.url + '/';
+  public datas: Testimonial[] = [];
+  public totalResult: number = 0;
+  public total: number = 0;
+  public loading: boolean = true;
+  public event: any;
+  public search_input: string = '';
 
-  constructor() { }
+  constructor(
+    private testimonialService: TestimonialService,
+    private title: Title,
+    private confirmationService: ConfirmationService,
+    private toastService: ToastService,
+  ) {
+    this.title.setTitle('Danh sách Khách hàng chứng thực');
+  }
 
   ngOnInit(): void {
   }
 
+  public load(event: LazyLoadEvent) {
+    this.setLoadingStatus(true);
+    this.event = event;
+    this.event.searchInput = this.search_input;
+    setTimeout(() => {
+      this.loadData();
+    }, 1000);
+  }
+
+  public search(value: string) {
+    this.setLoadingStatus(true);
+    this.search_input = value;
+    this.event.searchInput = this.search_input;
+    this.loadData();
+  }
+
+  private loadData() {
+    this.testimonialService.list(this.event).subscribe((response) => {
+      this.datas = response.data;
+      this.totalResult = response.total_result;
+      this.total = response.total;
+      this.setLoadingStatus(false);
+    });
+  }
+
+  public delete(id: number) {
+    this.confirmationService.confirm({
+      message: 'Bạn có muốn xoá?',
+      header: 'Xoá',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.testimonialService.delete(id).subscribe((response) => {
+          if (response.success == 1) {
+            this.toastService.success('Thành công', response.message);
+            this.setLoadingStatus(true);
+            this.loadData();
+          } else {
+            this.toastService.error('Lỗi', response.message);
+          }
+        });
+      },
+      reject: () => {
+
+      },
+      key: "positionDialog"
+    });
+  }
+
+  private setLoadingStatus(status: boolean) {
+    this.loading = status;
+  }
 }
