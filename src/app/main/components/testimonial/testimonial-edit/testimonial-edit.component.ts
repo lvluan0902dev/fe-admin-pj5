7908a707c97da6a6549c5b4ef977a5602f58fd02
leluan <levanluan090201>
2022-11-07 15:11:46 +0700

    Testimonial - Complete edit function

--- src/app/main/components/testimonial/testimonial-edit/testimonial-edit.component.ts
+++ src/app/main/components/testimonial/testimonial-edit/testimonial-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { Testimonial } from 'src/app/main/models/testimonial/testimonial.model';
+import { TestimonialService } from 'src/app/main/services/testimonial/testimonial.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-testimonial-edit',
@@ -6,10 +13,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonial-edit.component.css']
 })
 export class TestimonialEditComponent implements OnInit {
+  public form = this.fb.group({
+    name: ['', [Validators.required]],
+    content: ['', [Validators.required]],
+    image: ['', []],
+    status: [true, []],
+  });
 
-  constructor() { }
+  public submitted: boolean = false;
+  public url = environment.url + '/';
+  private id: any;
+  private testimonial!: Testimonial;
+  public image_name: string = '';
+  public image_path: string = '';
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private title: Title,
+    private fb: FormBuilder,
+    private toastService: ToastService,
+    private router: Router,
+    private testimonialService: TestimonialService,
+  ) {
+    this.title.setTitle('Sửa slider');
   }
 
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.get();
+  }
+
+  public get() {
+    this.testimonialService.get(this.id).subscribe((response) => {
+      if (response.success == 1) {
+        this.testimonial = response.data;
+        this.setImage(this.testimonial.image_name, this.testimonial.image_path);
+        this.setForm(this.testimonial);
+      } else {
+        this.toastService.error('Error', response.message);
+      }
+    });
+  }
+
+  private setForm(data: any) {
+    this.form = this.fb.group({
+      name: [data.name, [Validators.required]],
+      content: [data.content, [Validators.required]],
+      image: ['', []],
+      status: [data.status == 1 ? true : false, []],
+    });
+  }
+
+  private setImage(image_name: string, image_path: string) {
+    this.image_name = image_name;
+    this.image_path = image_path;
+  }
+
+  public save(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+
+      // Set PUT method
+      formData.append('_method', 'PUT');
+
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      formData.append('id', String(this.testimonial.id));
+
+      this.testimonialService.edit(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.submitted = false;
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
+
+  public saveAndBack(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+
+      // Set PUT method
+      formData.append('_method', 'PUT');
+
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      formData.append('id', String(this.testimonial.id));
+
+      this.testimonialService.edit(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.router.navigateByUrl('/testimonial/list');
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
 }
