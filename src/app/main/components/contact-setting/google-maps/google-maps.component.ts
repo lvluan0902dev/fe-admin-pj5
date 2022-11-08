e31b4417bd29a7bc97b0d3c394cdf7ca7061b0e1
leluan <levanluan090201>
2022-11-08 11:32:20 +0700

    ContactSetting - Completed email, google maps

--- src/app/main/components/contact-setting/google-maps/google-maps.component.ts
+++ src/app/main/components/contact-setting/google-maps/google-maps.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { ContactSetting } from 'src/app/main/models/contact-setting/contact-setting.model';
+import { ContactSettingService } from 'src/app/main/services/contact-setting/contact-setting.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-google-maps',
@@ -6,10 +13,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./google-maps.component.css']
 })
 export class GoogleMapsComponent implements OnInit {
+  public form = this.fb.group({
+    content: ['', [Validators.required]],
+    status: [true, []],
+  });
 
-  constructor() { }
+  public submitted: boolean = false;
+  public url = environment.url + '/';
+  private title_obj: any;
+  private contactSetting!: ContactSetting;
+  public image_name: string = '';
+  public image_path: string = '';
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private title: Title,
+    private fb: FormBuilder,
+    private toastService: ToastService,
+    private router: Router,
+    private contactSettingService: ContactSettingService,
+  ) {
+    this.title.setTitle('Google Maps');
   }
 
+  ngOnInit(): void {
+    this.title_obj = 'google_maps';
+    this.get();
+  }
+
+  public get() {
+    this.contactSettingService.get(this.title_obj).subscribe((response) => {
+      if (response.success == 1) {
+        this.contactSetting = response.data;
+        this.setForm(this.contactSetting);
+      } else {
+        this.toastService.error('Error', response.message);
+      }
+    });
+  }
+
+  private setForm(data: any) {
+    this.form = this.fb.group({
+      content: [data.content, [Validators.required]],
+      status: [data.status == 1 ? true : false, []],
+    });
+  }
+
+  private setImage(image_name: string, image_path: string) {
+    this.image_name = image_name;
+    this.image_path = image_path;
+  }
+
+  public save(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+
+      // Set PUT method
+      formData.append('_method', 'PUT');
+
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      formData.append('id', String(this.contactSetting.id));
+
+      this.contactSettingService.edit(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.submitted = false;
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
 }
