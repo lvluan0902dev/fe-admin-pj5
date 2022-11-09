1e8cc09d7ac386865626bd665b2e14fa2b214aa4
leluan <levanluan090201>
2022-11-09 10:21:02 +0700

    ProductCategory - Completed add and edit function

--- src/app/main/components/product-category/product-category-add/product-category-add.component.ts
+++ src/app/main/components/product-category/product-category-add/product-category-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { ProductCategoryService } from 'src/app/main/services/product-category/product-category.service';
 
 @Component({
   selector: 'app-product-category-add',
@@ -6,10 +11,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-category-add.component.css']
 })
 export class ProductCategoryAddComponent implements OnInit {
+  public form = this.fb.group({
+    name: ['', [Validators.required]],
+    status: [true, []],
+  });
 
-  constructor() { }
+  public submitted: boolean = false;
+
+  constructor(
+    private title: Title,
+    private fb: FormBuilder,
+    private productCategoryService: ProductCategoryService,
+    private toastService: ToastService,
+    private router: Router
+  ) {
+    this.title.setTitle('Thêm Danh mục sản phẩm');
+  }
 
   ngOnInit(): void {
   }
 
+  public save(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      this.productCategoryService.add(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.submitted = false;
+          this.form.reset();
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
+
+  public saveAndBack(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      this.productCategoryService.add(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.router.navigateByUrl('/product-category/list');
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
 }
--- src/app/main/components/product-category/product-category-edit/product-category-edit.component.ts
+++ src/app/main/components/product-category/product-category-edit/product-category-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { ProductCategory } from 'src/app/main/models/product-category/product-category.model';
+import { ProductCategoryService } from 'src/app/main/services/product-category/product-category.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-product-category-edit',
@@ -6,10 +13,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-category-edit.component.css']
 })
 export class ProductCategoryEditComponent implements OnInit {
+  public form = this.fb.group({
+    name: ['', [Validators.required]],
+    status: [true, []],
+  });
 
-  constructor() { }
+  public submitted: boolean = false;
+  public url = environment.url + '/';
+  private id: any;
+  private productCategory!: ProductCategory;
+  public image_name: string = '';
+  public image_path: string = '';
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private title: Title,
+    private fb: FormBuilder,
+    private toastService: ToastService,
+    private router: Router,
+    private productCategoryService: ProductCategoryService,
+  ) {
+    this.title.setTitle('Sửa Danh mục sản phẩm');
   }
 
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.get();
+  }
+
+  public get() {
+    this.productCategoryService.get(this.id).subscribe((response) => {
+      if (response.success == 1) {
+        this.productCategory = response.data;
+        this.setForm(this.productCategory);
+      } else {
+        this.toastService.error('Error', response.message);
+      }
+    });
+  }
+
+  private setForm(data: any) {
+    this.form = this.fb.group({
+      name: [data.name, [Validators.required]],
+      status: [data.status == 1 ? true : false, []],
+    });
+  }
+
+  private setImage(image_name: string, image_path: string) {
+    this.image_name = image_name;
+    this.image_path = image_path;
+  }
+
+  public save(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+
+      // Set PUT method
+      formData.append('_method', 'PUT');
+
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      formData.append('id', String(this.productCategory.id));
+
+      this.productCategoryService.edit(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.submitted = false;
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
+
+  public saveAndBack(files: FileList | null) {
+    this.submitted = true;
+    if (this.form.status == 'VALID') {
+      var formData = new FormData();
+
+      // Set PUT method
+      formData.append('_method', 'PUT');
+
+      var data = this.form.value as any;
+      for (let key of Object.keys(data)) {
+        formData.append(key, data[key])
+      }
+      if (files?.length) {
+        formData.set('image', files[0]);
+      }
+
+      formData.append('id', String(this.productCategory.id));
+
+      this.productCategoryService.edit(formData).subscribe((response) => {
+        if (response.success == 1) {
+          this.toastService.success('Thành công', response.message);
+          this.router.navigateByUrl('/product-category/list');
+        } else {
+          this.toastService.error('Lỗi', response.message);
+        }
+      });
+    } else {
+      // Do something
+    }
+  }
 }
