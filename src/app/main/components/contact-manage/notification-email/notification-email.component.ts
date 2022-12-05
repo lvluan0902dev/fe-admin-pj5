5232b62fa032d47e150df6663c401d7da0df7d02
leluan <levanluan090201>
2022-12-05 08:38:38 +0700

    ContactManage - Completed

--- src/app/main/components/contact-manage/notification-email/notification-email.component.ts
+++ src/app/main/components/contact-manage/notification-email/notification-email.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ConfirmationService, LazyLoadEvent } from 'primeng/api';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { NotificationEmail } from 'src/app/main/models/notification-email/notification-email.model';
+import { ContactManageService } from 'src/app/main/services/contact-manage/contact-manage.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-notification-email',
@@ -6,10 +12,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-email.component.css']
 })
 export class NotificationEmailComponent implements OnInit {
+  public url = environment.url + '/';
+  public datas: NotificationEmail[] = [];
+  public totalResult: number = 0;
+  public total: number = 0;
+  public loading: boolean = true;
+  public event: any;
+  public search_input: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private contactManageService: ContactManageService,
+    private title: Title,
+    private confirmationService: ConfirmationService,
+    private toastService: ToastService,
+  ) {
+    this.title.setTitle('Danh sách Email thông báo');
   }
 
+  ngOnInit(): void {
+
+  }
+
+  public load(event: LazyLoadEvent) {
+    this.setLoadingStatus(true);
+    this.event = event;
+    this.event.searchInput = this.search_input;
+    setTimeout(() => {
+      this.loadData();
+    }, 1000);
+  }
+
+  public search(value: string) {
+    this.setLoadingStatus(true);
+    this.search_input = value;
+    this.event.searchInput = this.search_input;
+    this.loadData();
+  }
+
+  private loadData() {
+    this.contactManageService.listNotificationEmail(this.event).subscribe((response) => {
+      this.datas = response.data;
+      this.totalResult = response.total_result;
+      this.total = response.total;
+      this.setLoadingStatus(false);
+    });
+  }
+
+  public delete(id: number) {
+    this.confirmationService.confirm({
+      message: 'Bạn có muốn xoá?',
+      header: 'Xoá',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.contactManageService.deleteNotificationEmail(id).subscribe((response) => {
+          if (response.success == 1) {
+            this.toastService.success('Thành công', response.message);
+            this.setLoadingStatus(true);
+            this.loadData();
+          } else {
+            this.toastService.error('Lỗi', response.message);
+          }
+        });
+      },
+      reject: () => {
+
+      },
+      key: "topDialog"
+    });
+  }
+
+  private setLoadingStatus(status: boolean) {
+    this.loading = status;
+  }
 }
---
+++ src/app/main/models/notification-email/notification-email.model.ts
@@ -0,0 +1,6 @@
+export interface NotificationEmail {
+    id: number,
+    email: string,
+    created_at: string,
+    updated_at: string
+}
