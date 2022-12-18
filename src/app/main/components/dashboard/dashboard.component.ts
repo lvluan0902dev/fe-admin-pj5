edc39235de44d35a25aadd33c3fd2af492023960
Le Van Luan <le.van.luan@quape.com>
2022-12-18 14:58:36 +0700

    Dashboard - Complete widget 1, 3

--- src/app/main/components/dashboard/dashboard.component.ts
+++ src/app/main/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { DashboardService } from '../../services/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,21 +9,93 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public totalProduct: number = 0;
+  public totalBlog: number = 0;
+  public totalOrder: number = 0;
+  public totalMessage: number = 0;
+  public totalOrderNew: number = 0;
+  public totalOrderTransport: number = 0;
+  public totalOrderDone: number = 0;
+  public totalOrderCancel: number = 0;
 
   constructor(
     private authService: AuthService,
-    private title: Title
+    private title: Title,
+    private dashboardService: DashboardService
   ) {
     this.title.setTitle('Dashboard');
   }
 
   ngOnInit(): void {
     this.me();
+    this.getProductCount();
+    this.getBlogCount();
+    this.getOrderCount();
+    this.getMessageCount();
+    this.getOrderStatusCount();
   }
 
   public me() {
     this.authService.me().subscribe((data) => {
       console.log(data);
     })
+  }
+
+  private getProductCount() {
+    this.dashboardService.getProductCount().subscribe((response) => {
+      if (response.success == 1) {
+        this.totalProduct = response.data;
+      }
+    });
+  }
+
+  private getBlogCount() {
+    this.dashboardService.getBlogCount().subscribe((response) => {
+      if (response.success == 1) {
+        this.totalBlog = response.data;
+      }
+    });
+  }
+
+  private getOrderCount() {
+    this.dashboardService.getOrderCount().subscribe((response) => {
+      if (response.success == 1) {
+        this.totalOrder = response.data;
+      }
+    });
+  }
+
+  private getMessageCount() {
+    this.dashboardService.getMessageCount().subscribe((response) => {
+      if (response.success == 1) {
+        this.totalMessage = response.data;
+      }
+    });
+  }
+
+  private getOrderStatusCount() {
+    this.dashboardService.getOrderStatusCount(0).subscribe((response) => {
+      if (response.success == 1) {
+        this.totalOrderNew = response.data;
+      }
+    });
+
+    this.dashboardService.getOrderStatusCount(1).subscribe((response) => {
+      if (response.success == 1) {
+        this.totalOrderTransport = response.data;
+      }
+    });
+
+    this.dashboardService.getOrderStatusCount(2).subscribe((response) => {
+      if (response.success == 1) {
+        this.totalOrderDone = response.data;
+      }
+    });
+
+    this.dashboardService.getOrderStatusCount(3).subscribe((response) => {
+      if (response.success == 1) {
+        this.totalOrderCancel = response.data;
+      }
+    });
   }
 }
---
+++ src/app/main/services/dashboard/dashboard.service.ts
@@ -0,0 +1,44 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpService } from 'src/app/core/services/http/http.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private url = 'admin/dashboard/';
+
+  constructor(
+    private httpService: HttpService
+  ) { }
+
+  public getProductCount() {
+    return this.httpService.get(this.url + 'get-product-count', httpOptions);
+  }
+
+  public getBlogCount() {
+    return this.httpService.get(this.url + 'get-blog-count', httpOptions);
+  }
+
+  public getOrderCount() {
+    return this.httpService.get(this.url + 'get-order-count', httpOptions);
+  }
+
+  public getMessageCount() {
+    return this.httpService.get(this.url + 'get-message-count', httpOptions);
+  }
+
+  /**
+   * 
+   * @param id - order id
+   * @returns 
+   */
+  public getOrderStatusCount(id: any) {
+    return this.httpService.get(this.url + 'get-order-status-count/' + id, httpOptions);
+  }
+}
